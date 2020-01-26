8b1a43c5fcd86ce3ea689391869872a084d2536d
Alvarsson <a-alvarsson@hotmail.com>
2020-01-26 22:10:33 +0100

    pushing latest fixes, still doesn't load node graph though

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,14 +1,29 @@
 from flask import render_template, url_for, Flask
 from app.main import main_bp as bp
 import requests
+import json
+import networkx as nx
+from networkx.readwrite import json_graph
+import matplotlib.pyplot as plt
+import os
+	
+def load_json():
+	filename = os.path.join(os.path.dirname(__file__), '../static', 'network.json')
+	print(filename)
+	with open(filename) as node_graph:
+		data = json.load(node_graph)
+	return data
 
 @bp.route('/')
 def index():
-	return 'Detta är startsidan'
+	node_data = load_json()
+	print("hej")
+	print(node_data)
+	return render_template("index.html", node_data=node_data)
 
 
-@bp.route('/hejsan')
+@bp.route('/network')
 def index2():
-	return 'Detta är andra sidan'
+	return render_template('/network.json')
 
 
