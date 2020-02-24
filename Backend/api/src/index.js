8954d4b3110569c97227f2f49af3f6ab092b1b11
Peter Larsson <peter@kodifiera.se>
2020-02-24 14:04:04 +0100

    Made backend to run with docker(-compose)

--- Backend/api/src/index.js
+++ Backend/api/src/index.js
@@ -24,11 +24,12 @@ const schema = makeAugmentedSchema({
  * using credentials specified as environment variables
  * with fallback to defaults
  */
+
 const driver = neo4j.driver(
   process.env.NEO4J_URI || "bolt://localhost:7687",
   neo4j.auth.basic(
     process.env.NEO4J_USER || "neo4j",
-    process.env.NEO4J_PASSWORD || "neo4j"
+    process.env.NEO4J_PASSWORD || "letmein"
   )
 );
 
